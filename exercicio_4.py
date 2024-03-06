4c640a81b4dc74a8a4b87a4f754d1efc513d88af
Thiago Pinotti <thiago.pinotti@gmail.com>
2024-03-06 00:39:53 -0300

    ganbatte

---
+++ exercicio_4.py
@@ -0,0 +1,19 @@
+"""
+4) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em uma sala diferente.
+Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser.
+Seu objetivo é descobrir qual interruptor controla qual lâmpada.
+
+Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?
+"""
+
+# Resposta.
+
+"""
+
+Ligaria um interruptor (1) por um tempo e o desligaria, ai eu ligaria o interruptor (2) e iria verificar a sala.
+
+a lampada quente seria o interruptor 1, a ligada o interruptor 2 e a apagada o interruptor 3.
+
+"""
+
+
---
+++ exercicio_5.py
@@ -0,0 +1,14 @@
+"""
+5) Escreva um programa que inverta os caracteres de um string.
+IMPORTANTE:
+
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+
+b) Evite usar funções prontas, como, por exemplo, reverse;
+
+"""
+
+palavra = "me arruma um estágio"
+nova_palavra = palavra[::-1]
+
+print(nova_palavra)
